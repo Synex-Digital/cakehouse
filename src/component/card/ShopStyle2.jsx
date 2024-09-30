073aa6d7d46410039ac2ede972f9dfcf1472c69b
md-parves <parves.dev@gmail.com>
2024-09-30 11:36:42 +0600

    add Shop Style 2

---
+++ src/component/card/ShopStyle2.jsx
@@ -0,0 +1,50 @@
+import React from "react";
+import { FaStar } from "react-icons/fa";
+
+const ShopCard = ({
+  imageSrc,
+  title,
+  description,
+  price,
+  isTopSeller,
+  rating,
+}) => {
+  return (
+    <li className="lg:w-1/3 md:w-1/2 w-full px-[15px] mb-[30px] All drink sweet salad">
+      <div className="dz-img-box7 rounded-[10px] bg-white text-center relative h-full duration-200 overflow-hidden z-[1] shadow-[0px_15px_55px_rgba(34,34,34,0.15)]">
+        <div className="dz-media relative overflow-hidden">
+          <img style={{
+            width:"270px",
+            height:"162px"
+          }} src={imageSrc} alt={title} className="duration-300" />
+          <div className="dz-meta">
+            <ul>
+              {isTopSeller && (
+                <li className="absolute top-0 bg-[#FE9F10] left-0 text-white rounded-ee-[10px] uppercase py-[4px] px-2.5 font-semibold text-xs leading-[18px] z-[2]">
+                  Top Seller
+                </li>
+              )}
+              {rating && (
+                <li className="rating absolute bottom-[20px] right-[20px] bg-white text-[#222222] rounded-md text-sm font-medium py-1 px-[10px] mr-0 flex items-center">
+                  <i className="fa-solid fa-star text-[#FE9F10] text-xs top-[-2px] mr-[5px] relative scale-[1.2]"><FaStar /></i>{" "}
+                  {rating}
+                </li>
+              )}
+            </ul>
+          </div>
+        </div>
+        <div className="dz-content flex flex-col lg:py-[25px] py-5 lg:px-5 px-[15px]">
+          <h5 className="title text-black2 mb-2">
+            <a href="product-detail.html">{title}</a>
+          </h5>
+          <p className="mb-[10px] text-sm">{description}</p>
+          <span className="price text-2xl font-semibold leading-[1.1]">{`$${Math.ceil(
+            price
+          )}`}</span>
+        </div>
+      </div>
+    </li>
+  );
+};
+
+export default ShopCard;
